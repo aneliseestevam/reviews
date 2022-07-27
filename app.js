c52dda738e936f819abc93ce44b69020bf88754d
Anelise Estevam <aestevam.dev@gmail.com>
2022-07-26 23:20:35 -0300

    Add CurrentItem variable

--- app.js
+++ app.js
@@ -40,6 +40,9 @@ const prevBtn = document.querySelector(".prev-btn");
 const nextBtn = document.querySelector(".next-btn");
 const randomBtn = document.querySelector(".random-btn");
 
+// set starting item
+let currentItem = 0;
+
 // load initial item
 window.addEventListener("DOMContentLoaded", function () {
   showPerson(currentItem);
